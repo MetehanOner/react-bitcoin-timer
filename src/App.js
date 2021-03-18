db27ba209aac6a25d4372dca3ccffe8d9d3440ff
Metehan <m3t3b00s@gmail.com>
2021-03-18 17:39:45 +0300

    initial commit for bitcoin timer app for 1 million countdown

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
+import {useEffect, useState} from 'react';
+
+const BINANCE_API_LINK = 'wss://stream.binance.com:9443/ws/btcusdt@trade';
+
 
 function App() {
+
+  const [data, setData] = useState([]);
+
+  
+    useEffect(() => {
+      const socketci = new WebSocket(BINANCE_API_LINK);
+      socketci.addEventListener('message', function (event) {
+        let messageObject = JSON.parse(event.data);
+        //console.log('Message from server ', messageObject.p);
+        setData(param => [...param, messageObject]);
+        //console.log('every put fine')
+      });
+
+      
+    }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+     <ul>
+      {data.map((datas, index) => (<li key={index}>{datas.p}</li>))
+      }
+     </ul>
     </div>
   );
 }
